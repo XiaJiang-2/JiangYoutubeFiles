e43244ba48605ad04d55414c05056bb16dab12a4
Xia Jiang <xij6.gm@gmail.com>
2021-01-12 09:57:49 -0500

    New version of lr_torch.py

--- lr_torch.py
+++ lr_torch.py
@@ -1,30 +1,26 @@
+#!/usr/bin/env python
+# coding: utf-8
+# %load "~/lr_torch.py"
 
 '''
-A script that demonstrates how we can use gradient descent to optimize
-the parameters of a linear regression model.
+A script solely for the purpose of demonstrating how we can use gradient 
+descent to optimize the parameters of a linear regression model. No error-handling
+was considered when writing the script.
 author: Xia Jiang
 date:2021.1.8
 #! /usr/bin/env python
 '''
-
 import torch.nn as nn
 import torch
-x = torch.tensor([1,2,3,4,4,5,6,6,7,8])
-y= torch.tensor ([2,3,4,3,4,6,5,7,8,8])
-x=x.float()
-x =x.reshape(-1,1)
-y=y.float()
-y=y.reshape(-1,1)
-torch.manual_seed(5)
-model = nn.Linear(1,1)
+
 def getparas (model):
     [w1, w0] =model.parameters()
     return (w1,w0)
-crit = torch.nn.MSELoss()
-optm= torch.optim.SGD(model.parameters(),lr=0.03)
-num_epochs= 2000
-
-def model_run(epochs, model,x, y, crit, optm):
+def model_run(epochs, lr, x, y, seed):
+    torch.manual_seed(seed)
+    model = nn.Linear(1,1)
+    crit = torch.nn.MSELoss()
+    optm= torch.optim.SGD(model.parameters(),lr=lr)
     for i in range(epochs):
         optm.zero_grad()
         pred_y_values = model(x)
@@ -33,10 +29,16 @@ def model_run(epochs, model,x, y, crit, optm):
         cur_loss.backward()
         optm.step()
     return model
-print(__name__)
+#example for usering model_run to train and return a linear model
+x = torch.tensor([1,2,3,4,4,5,6,6,7,8])
+y= torch.tensor ([2,3,4,3,4,6,5,7,8,8])
+x=x.float()
+x =x.reshape(-1,1)
+y=y.float()
+y=y.reshape(-1,1)
+num_epochs=1000
+lr=0.01
+seed = 5
 if __name__=="__main__":
-    model_run(num_epochs, model, x, y, crit, optm)
+    model=model_run(num_epochs, lr, x, y, seed)
     print(getparas(model))
-
-
-
